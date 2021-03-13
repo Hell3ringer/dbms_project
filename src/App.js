d782b3bb945570dbe1bf817fc0aa15889701dc47
kedar <kedar.kanday@gmail.com>
2021-03-13 12:59:49 +0530

    commiting to dev

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Login from './components/login.component'
+import SignUp from './components/signup.component'
+import {Route, Switch} from 'react-router-dom'
+import error from './components/error'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {<Switch>
+        <Route exact path='/' component={Login}/>
+        <Route exact path='/sign-up' component={SignUp}/>
+        <Route component={error}/>
+      </Switch>}
     </div>
   );
 }
---
+++ src/components/error.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const error=()=>{
+    return <h1>error page</h1>
+}
+export default error
