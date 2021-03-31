22d6916d00cf220b7364f77e4891a4a0b4b9fad8
kedar <kedar.kanday@gmail.com>
2021-03-31 13:10:23 +0530

    sidebar and topnav working now

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
-import topnav from '../dashboard/topnav'
-import sidebar from '../dashboard/sidebar'
+import Topnav from '../dashboard/topnav'
+import Sidebar from '../dashboard/sidebar'
 
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../stylesheets/profile.css'
+
 
 class profile extends Component{
     render(){
         return(
             <div className="entire_div_profile">
                 {/* <Dashboard/> */}
-                <topnav/>
-                <sidebar/>
+                <Topnav/>
+                <Sidebar/>
                 <div className="side_main_box">
                     <p>This is profile page OF STUDENT</p><br></br>
                     <p>window loc is {window.location.pathname}</p>
