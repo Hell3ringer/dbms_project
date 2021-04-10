39148dbd316af0a4b1a37a082daa99684df5d811
kedar <kedar.kanday@gmail.com>
2021-04-10 09:51:53 +0530

    added constraint 300

--- src/components/register_into_courses/courses_table.js
+++ src/components/register_into_courses/courses_table.js
@@ -3,12 +3,10 @@ import React, { Component } from 'react'
 import Topnav from '../dashboard/topnav'
 import Sidebar from '../dashboard/sidebar'
 import Swal from 'sweetalert2'
-import {FaSearch} from 'react-icons/fa'
  
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../stylesheets/profile.css'
 import axios from 'axios';
-
  
 // async function getCourses(){
 //     let res=await axios.get('http://localhost:4000/app/courses')
@@ -31,8 +29,8 @@ class courses_table extends Component{
             courses:[],
             id:'',
             enrolled_courses:[],
-            s_id:'2019A7PS0155H',
-            seach_course:''    //HARD-CODED HERE
+            s_id:'2019A7PS0155H',  //HARD-CODED HERE
+            n:-1   
         }
     }
     getCourses(){
@@ -40,7 +38,6 @@ class courses_table extends Component{
         .then(res=>{
             this.setState({courses:res.data})
         })
-        console.log("got courses");
     }
     getEnrolledCourses(){
         const student={s_id:this.state.s_id}
@@ -54,7 +51,6 @@ class courses_table extends Component{
     componentDidMount(){
         this.getCourses();
         this.getEnrolledCourses();
-        document.getElementById("half_table").setAttribute('class','display_class')
     }
     get_td(c_id){
         // console.log(this.state.enrolled_courses);
@@ -116,6 +112,8 @@ class courses_table extends Component{
                 this.setState({id:c_id})
                         console.log('selected '+c_id+' by '+this.state.s_id);
                         const pair={c_id:c_id,s_id:this.state.s_id}
+                        const course={c_id:c_id}
+                        
                         if(this.state.enrolled_courses.length>=9){
                             Swal.fire({
                                 title:'Alert!!!',
@@ -125,32 +123,60 @@ class courses_table extends Component{
                             })
                         }
                         else{
-                        Swal.fire({
-                            text:'Are you sure, you want to register to '+c_id+'?',
-                            confirmButtonText:'Yes, Register',
-                            showCancelButton:true,
-                            cancelButtonText:'No, dont'
-                        }).then((result)=>{
-                            if(result.value){
-                                axios.post('http://localhost:4000/app/register_student',{pair})
-                                .then(Response=>{
-                                if(Response.status===200){
-                                    document.getElementById("alert_reg").innerHTML="Successfully registered to "+c_id;                               
-                                }
-                                })
+                            axios.post('http://localhost:4000/app/get_enrolled_students',{course})
+                            .then(Response=>{
+                                this.setState({n:Response.data.length})
+                                console.log("length is"+Response.data.length);
+                                console.log("n is"+this.state.n);
+                                
+                                    let n=this.state.n;
+                                    // axios.post('http://localhost:4000/app/get_enrolled_students',{course})
+                                    // .then(Response=>{
+                                    //     this.setState({n:Response.data.length})
+                                    //     // console.log("length is"+Response.data.length);
+                                    //     // console.log("n is"+this.state.n);
+                                    //     n=Response.data.length;
+                                    // })
+                                    console.log("n is"+n);
+                                if(n<300){
+                                let avail=300-n;
+                                console.log("avail is "+avail);
+                                console.log("n is "+n);
                                 Swal.fire({
-                                    title:'Registration',
-                                    text:'Successfully registered to '+c_id,
-                                    confirmButtonText:'Ok',
-                                    icon:'success'
-                                }).then((res)=>{
-                                    if(res.value){
-                                        window.location.reload();
+                                    html:'<div>Available seats: '+avail+' Maximum limit:300.<br>'+'Are you sure, you want to register to '+c_id+'?</div>',
+                                    confirmButtonText:'Yes, Register',
+                                    showCancelButton:true,
+                                    cancelButtonText:'No, dont'
+                                }).then((result)=>{
+                                    if(result.value){
+                                        axios.post('http://localhost:4000/app/register_student',{pair})
+                                        .then(Response=>{
+                                        if(Response.status===200){
+                                            document.getElementById("alert_reg").innerHTML="Successfully registered to "+c_id;                               
+                                        }
+                                        })
+                                        Swal.fire({
+                                            title:'Registration',
+                                            text:'Successfully registered to '+c_id,
+                                            confirmButtonText:'Ok',
+                                            icon:'success'
+                                        }).then((res)=>{
+                                            if(res.value){
+                                                window.location.reload();
+                                            }
+                                        })
                                     }
-                                })
-                            }
-                            
-                        });
+                                    
+                                });
+                                }
+                                else{
+                                    Swal.fire({
+                                        title:"Alert!",
+                                        text:"Maximum limit reached!, you cannot enroll into this course",
+                                        confirmButtonText:"Ok, got it"
+                                    })
+                                }
+                            })
                         }    
             }}>Register</button></td>)
         }
@@ -167,43 +193,24 @@ class courses_table extends Component{
                     <td>{c_id}</td>
                     <td>{c_name}</td>
                     <td>{credits}</td>
+                    {/* <td><button onClick={()=>{
+                        this.setState({id:c_id})
+                        console.log('selected '+c_id+' by '+this.state.s_id);
+                        const pair={c_id:c_id,s_id:this.state.s_id}
+                        axios.post('http://localhost:4000/app/register_student',{pair})
+                        .then(Response=>{
+                            if(Response.status===200){
+                                document.getElementById("alert_reg").innerHTML="Successfully registered to "+c_id;
+                            }
+                        })
+                    }                 
+                    }>Register</button></td> */}
                     {this.get_register(c_id)}
                     {this.get_td(c_id)}
                 </tr>
             )
         })
     }
-    renderTableData2(){
-        // console.log(search+"inside r2");
-        // var search=document.getElementById("search_bar").value;
-        // console.log(this.state.courses);
-        var search=this.state.seach_course;
-        console.log("search is "+search);
-        return this.state.courses.map((course,index)=>{
-            const {c_id,c_name,credits}=course
-            // console.log("c_id is "+c_id+" search is "+search);
-            let lower=JSON.stringify(c_id);
-            // console.log("lower is of type "+(lower));
-            var l=lower.toLowerCase();
-            // console.log("l is "+l);
-            // console.log("type of c_id is "+typeof(c_id)+" type of l is "+typeof(l));
-            if(c_id.startsWith(search)){   
-            return(
-                <tr id={c_id}>
-                    <td>{c_id}</td>
-                    <td>c_name</td>
-                    <td>{search}</td>
-                    {this.get_register(c_id)}
-                    {this.get_td(c_id)}
-                </tr>
-            )
-            }
-        })
-    }
- 
- 
-    
- 
     
     render(){
         return(
@@ -219,49 +226,6 @@ class courses_table extends Component{
                         <br></br>
                         <h3>Register Into Courses</h3><br></br><br></br>
                         <div id="alert_reg"></div>
-                        
-                        {/* <table id="courses_table" className="table table-bordered table-hover">
-                            <thead className="thead-dark">
-                            <tr>
-                                <th>Cid</th>
-                                <th>Cname</th>
-                                <th>Credits</th>
-                                <th>Register</th>
-                                <th>Unenroll</th>
-                            </tr>
-                            </thead>
-                            
-                            <tbody>
-                                
-                                {this.renderTableData()}
-                            </tbody>
-                        </table> */}
-                        <FaSearch></FaSearch><input
-                        style={{ marginLeft: 5 }}
-                        type="text"
-              placeholder="Type to search..."
-              id="search_bar"
-              onChange={e => {
-                  this.setState({seach_course:document.getElementById("search_bar").value})
-                  console.log(document.getElementById("search_bar").value);
-                    if(document.getElementById("search_bar").value===''){
-                        
-                        console.log("search is empty");
-                        // console.log(document.getElementById("courses_table"));
-                        document.getElementById("full_table").setAttribute('class','show_display')
- 
-                    }
-                    else{
-                        // document.getElementById("test").innerHTML=(<div>
-                        //     <h3>Hi</h3><h4>Hello</h4>
-                        // </div>)
-                        document.getElementById("full_table").setAttribute('class','display_class')
-                        document.getElementById("half_table").setAttribute('class','show_display')
-                    }
-              }}
- 
-            ></input>
-                        <div id="full_table">
                         <table id="courses_table" className="table table-bordered table-hover">
                             <thead className="thead-dark">
                             <tr>
@@ -274,38 +238,15 @@ class courses_table extends Component{
                             </thead>
                             
                             <tbody>
-                                
                                 {this.renderTableData()}
                             </tbody>
                         </table>
-                        </div>
-                        <div id="test"> </div>
-                        <div id="half_table">
-                        <table id="courses_table_2" className="table table-bordered table-hover">
-                            <thead className="thead-dark">
-                            <tr>
-                                <th>Cid</th>
-                                <th>Cname</th>
-                                <th>Credits</th>
-                                <th>Register</th>
-                                <th>Unenroll</th>
-                            </tr>
-                            </thead>
-                            
-                            <tbody>
-                                
-                                {/* {x=document.getElementById("search_bar").value} */}
-                                {this.renderTableData2()}
-                            </tbody>
-                        </table>
-                        </div>
                     </div>
                 </div>  
  
             </div>
         )
     }
-    
 }
  
 export default courses_table;
